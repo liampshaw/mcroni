709fe384f8df781e6d79bc7e95d911e0121b19cb
liampshaw <liam.philip.shaw@gmail.com>
2021-06-01 11:04:30 +0100

    arguments and returns for functions, added N to rev_comp

--- seqFunctions.py
+++ seqFunctions.py
@@ -1,16 +1,38 @@
 from Bio import SeqIO
 
+
 # Read fasta to dict
 def read_fasta(fasta_file):
-    '''Simply uses SeqIO to read in fasta as dict.'''
-    return(SeqIO.to_dict(SeqIO.parse(fasta_file, 'fasta'), lambda rec:rec.id))
+    '''Reads in fasta file as dict using SeqIO.
 
-# For reverse complementing
-alt_map = {'ins':'0'}
-complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    Args:
+        fasta_file (str)
+            Filename of fasta
+
+    Returns:
+        fasta_dict (dict)
+            Dictionary of sequences in fasta
+    '''
+    fasta_parsed = SeqIO.parse(fasta_file, 'fasta') # read in fasta
+    fasta_dict = SeqIO.to_dict(fasta_parsed, lambda rec:rec.id) # convert to dict
+    return(fasta_dict)
+
 
 def reverse_complement(seq):
-    '''Returns the reverse complement of a DNA sequence.'''
+    '''Returns the reverse complement of a DNA sequence.
+
+    Args:
+        seq (str)
+            String of bases (e.g. ATCCG)
+
+    Returns:
+        bases (str)
+            Reverse complemented string (e.g. CGGAT)
+    '''
+    # For reverse_complement function
+    alt_map = {'ins':'0'}
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    # Replace any insertions.
     for k,v in alt_map.items():
         seq = seq.replace(k,v)
     bases = list(seq)
