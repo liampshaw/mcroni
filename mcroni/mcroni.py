89cb8c9d24ca3bd5120765305cb4bb2d8b1634e4
liampshaw <liam.philip.shaw@gmail.com>
2021-12-14 14:52:11 +0000

    classifying upstream as well

--- mcroni/mcroni.py
+++ mcroni/mcroni.py
@@ -14,9 +14,9 @@ import numpy as np
 import math
 import logging
 
-import mcroni.seqFunctions as sf # for conda
+# import mcroni.seqFunctions as sf # for conda
 # for local usage
-#import seqFunctions as sf
+import seqFunctions as sf
 
 
 
@@ -24,11 +24,16 @@ def get_options():
     parser = argparse.ArgumentParser(description='Analyse the local genomic context of mcr-1.',
                                      prog='mcroni')
     input_group = parser.add_mutually_exclusive_group(required=True) # mutually exclusive group
+    output_group = parser.add_mutually_exclusive_group(required=False) # mutually exclusive group
     input_group.add_argument('--fasta', help='Fasta file') # either f or l, but not both
     input_group.add_argument('--filelist', help='Alternatively: a list of fasta files')
     parser.add_argument('--output', help='Output prefix', required=True)
-    parser.add_argument("-v", "--verbose", help="verbose output",
-                    action="store_true")
+    parser.add_argument('-v', '--verbose', help='verbose output',
+                    action='store_true')
+    output_group.add_argument('--force', help='Force overwriting of output files.',
+                    action='store_true', required=False)
+    output_group.add_argument('--append', help='Append to existing output files.',
+                                    action='store_true', required=False)
     return parser.parse_args()
 
 def exit_message(message):
@@ -144,7 +149,11 @@ def cut_region(fasta_file, upstream_bases=1255, downstream_bases=1867):
             mcr_1_downstream = sf.reverse_complement(contig_seq[downstream_cut_position:mcr_1_end-1])
     region_seq = mcr_1_upstream+mcr_1_seq+mcr_1_downstream
     mcr_1_relative_start = len(mcr_1_upstream)
-    return([mcr_1_contig, mcr_1_variant, mcr_1_start, mcr_1_strand, mcr_1_relative_start, region_seq]) # mcr_1_start is in bases on contig, mcr_1_relative_start is in python for region_seq
+    # Classifying upstream 'promoter region' variant
+    promoter_region = mcr_1_upstream[-75:]
+    promoter_variant = sf.classify_variant(promoter_region, sf.get_data('promoter-variants.fa'))
+    # Return the results
+    return([mcr_1_contig, mcr_1_variant, promoter_variant, mcr_1_start, mcr_1_strand, mcr_1_relative_start, region_seq]) # mcr_1_start is in bases on contig, mcr_1_relative_start is in python for region_seq
 
 def classify_components(region_seq):
     '''Classify the presence of the various components of the mcr-1 composite transposon.
@@ -247,12 +256,12 @@ def classify_ISApl1_presence(region_seq, mcr_1_relative_start):
         else:
             ISApl1_dict['downstream'] = ISApl1_summary_list[0]
         return(ISApl1_dict)
-        # Should be a
 
 
 # Header for output file
 output_header = ('\t').join(['FILE', 'ISOLATE', 'MCR1.CONTIG',
-                            'MCR1.START', 'MCR1.STRAND','MCR1.VARIANT', 'PLASMIDS.ON.MCR1.CONTIG', 'PLASMIDS.ELSEWHERE',
+                            'MCR1.START', 'MCR1.STRAND','MCR1.VARIANT', 'PROMOTER.VARIANT',
+                            'PLASMIDS.ON.MCR1.CONTIG', 'PLASMIDS.ELSEWHERE',
                             'ISAPL1.UPSTREAM.REL.START', 'ISAPL1.UPSTREAM.REL.END',
                             'ISAPL1.UPSTREAM.INTERNAL.START', 'ISAPL1.UPSTREAM.INTERNAL.END',
                             'ISAPL1.DOWNSTREAM.REL.START', 'ISAPL1.DOWNSTREAM.REL.END',
@@ -263,6 +272,10 @@ def main():
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
     fastas = []
+    type_of_file_writing = 'w'
+    if args.append:
+        type_of_file_writing = 'a'
+
     if args.filelist is not None:
         with open(args.filelist, 'r') as f:
             for line in f.readlines():
@@ -272,7 +285,26 @@ def main():
         fastas.append(str(args.fasta))
     output_table_summary = args.output+'_table.tsv'
     output_fasta = args.output+'_sequence.fa'
-    with open(output_table_summary, 'w') as output_file:
+    if os.path.exists(output_fasta):
+        logging.debug('\nWARNING: output fasta already exists')
+        if args.force:
+            os.remove(output_fasta)
+        if args.append:
+            logging.debug('\nAppending to existing file.')
+        else:
+            logging.debug('\nERROR: refusing to overwrite existing output table. If used --force, will append')
+            return
+
+    if os.path.exists(output_table_summary):
+        logging.debug('\nWARNING: output table already exists.')
+        if args.force:
+            os.remove(output_table_summary)
+        if args.append:
+            logging.debug('\nAppending to existing file.')
+        else:
+            logging.debug('\nERROR: refusing to overwrite existing output table.')
+            return
+    with open(output_table_summary, type_of_file_writing) as output_file:
         output_file.write(output_header+'\n') # Write the header of table
         for fasta_file in fastas:
             if not os.path.exists(fasta_file):
@@ -281,12 +313,16 @@ def main():
             fasta_name = re.sub('\\..*', '', re.sub('.*\\/', '', fasta_file))
             output = cut_region(fasta_file)
             if output!=None:
-                mcr_1_contig, mcr_1_variant, mcr_1_start, mcr_1_strand, mcr_1_relative_start, region_seq = output[0], output[1], output[2], output[3], output[4], output[5]
+                mcr_1_contig, mcr_1_variant, promoter_variant, mcr_1_start, mcr_1_strand, mcr_1_relative_start, region_seq = output[0], output[1], output[2], output[3], output[4], output[5], output[6]
                 # Write to file
-                output_file.write('%s\t%s\t%s\t%s\t%s\t%s' % (fasta_file, fasta_name, mcr_1_contig, mcr_1_start, mcr_1_strand, mcr_1_variant))
+                output_file.write('%s\t%s\t%s\t%s\t%s\t%s\t%s' % (fasta_file, fasta_name, mcr_1_contig, mcr_1_start, mcr_1_strand, mcr_1_variant, promoter_variant))
                 # Write sequence too
-                with open(output_fasta, 'a') as output_fasta_file:
-                    output_fasta_file.write('>%s %s %s\n%s\n' % (fasta_name, mcr_1_contig, mcr_1_variant, region_seq))
+                if not os.path.exists(output_fasta):
+                    with open(output_fasta, 'w') as output_fasta_file:
+                        output_fasta_file.write('>%s %s %s\n%s\n' % (fasta_name, mcr_1_contig, mcr_1_variant, region_seq))
+                else:
+                    with open(output_fasta, 'a') as output_fasta_file:
+                        output_fasta_file.write('>%s %s %s\n%s\n' % (fasta_name, mcr_1_contig, mcr_1_variant, region_seq))
                 # Get plasmid replicons too
                 logging.debug('Finding plasmid replicons using abricate...')
                 plasmids = sf.plasmid_replicons(fasta_file, mcr_1_contig)
@@ -299,6 +335,8 @@ def main():
                         ISApl1_status['downstream'][0][0], ISApl1_status['downstream'][0][1], ISApl1_status['downstream'][1][0], ISApl1_status['downstream'][1][1]))
             else:
                 output_file.write('%s\t%s\t%s\n' % (fasta_file, fasta_name, '\t'.join(['NA' for i in range(0,14)]))) # 14 empty fields
+    if os.path.exists('tmp.fa'): # Clean up tmp file
+        os.remove('tmp.fa')
 
 if __name__ == "__main__":
     main()
